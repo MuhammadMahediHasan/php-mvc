b7936e3c2f60044f2c207ccd29de3d1304c57001
Mahedi <mahedi.hasan@skylarksoft.com>
2025-02-06 10:10:28 +0600

    Validator Enhancement

--- resource/views/layout/header.php
+++ resource/views/layout/header.php
@@ -7,7 +7,6 @@
 
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" type="text/css" href="<?php echo asset('lib/dataTables/datatables.min.css') ?>"/>
         <link rel="icon" href="<?php echo asset('img/favicon.ico')?>" type="image/x-icon">
         <link rel="stylesheet" href="<?php echo asset('lib/bootstrap/css/bootstrap.css')?>"/>
         <link rel="stylesheet" href="<?php echo asset('css/style.css')?>"/>
